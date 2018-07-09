a3d696ace8edd1d2e2af7eb85a2c9ad1a8161ce6
Ilya Semenov <semenovio@gmail.com>
2018-07-10 02:02:15 +0300

    JavaScript Part 1 in progress

---
+++ JavaScript_Level_One/Part3_solution.js
@@ -0,0 +1,2 @@
+alert("Your weight in kilos: " + prompt("Enter your weight in lbs") * 0.454)
+console.log("Conversion complete")
---
+++ JavaScript_Level_One/example.js
@@ -0,0 +1,5 @@
+alert("Hell is already there!")
+
+var deposit = prompt("Enter deposit sum")
+alert("You deposited: " + deposit)
+console.log("Cool!")
