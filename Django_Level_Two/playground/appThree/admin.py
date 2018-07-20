912972f3030531844c68517b6e3fef7f4f4d7ff8
Семенов Илья <semenov@stb.ua>
2018-07-20 17:17:06 +0300

    L2 project complete

--- Django_Level_Two/playground/appThree/admin.py
+++ Django_Level_Two/playground/appThree/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from appThree.models import AccessRecord, Topic, Webpage
+from appThree.models import AccessRecord, Topic, Webpage, User
 # Register your models here.
 admin.site.register(AccessRecord)
 admin.site.register(Topic)
 admin.site.register(Webpage)
+admin.site.register(User)
