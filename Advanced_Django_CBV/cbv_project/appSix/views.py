75c521ccfd0c68eedb2786823afb543cf9d896ba
Ilya Semenov <semenovio@gmail.com>
2018-07-30 00:40:26 +0300

    CBV project complete

--- Advanced_Django_CBV/cbv_project/appSix/views.py
+++ Advanced_Django_CBV/cbv_project/appSix/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import (TemplateView, ListView,
+                                    DetailView, CreateView, UpdateView,
+                                    DeleteView)
+from django.urls import reverse_lazy
 from . import models
 
 class IndexView(TemplateView):
@@ -18,3 +21,15 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'appSix/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    model = models.School
+    fields = ('name', 'principal', 'location')
+
+class SchoolUpdateView(UpdateView):
+    model = models.School
+    fields = ('name', 'principal')
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('appSix:list')
