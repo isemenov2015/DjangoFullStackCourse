cfe48b94cb5b1290ed10901e94a5d8ea7871ddda
Ilya Semenov <semenovio@gmail.com>
2018-08-16 02:07:40 +0300

    All projects complete

--- Django_Level_Two/ProTwo/ProTwo/urls.py
+++ Django_Level_Two/ProTwo/ProTwo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
 from appTwo import views
 
 urlpatterns = [
@@ -23,3 +24,9 @@ urlpatterns = [
     url(r'^users/',include('appTwo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
